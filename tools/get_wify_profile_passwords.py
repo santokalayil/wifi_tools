73214f92405bf37248649bf6c30272d7bdffa91b
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-04-26 19:44:25 +0530

    committing files

---
+++ tools/get_wify_profile_passwords.py
@@ -0,0 +1,31 @@
+import re
+import subprocess
+import json
+
+from tools.commands import cmd_show_wlan_profiles
+
+get_output = lambda cmd_list: subprocess.run(cmd_list,capture_output=True).stdout.decode()
+
+profile_names = (re.findall("All User Profile     : (.*)\r", get_output(cmd_show_wlan_profiles.split())))
+
+
+def get_full_wify_profile(profile_name):
+    wifi_profile = {}
+    cmd_list_1 = "netsh wlan show profile".split() + [profile_name]
+    if re.search("Security key           : Absent", get_output(cmd_list_1)):
+        pass
+    else:
+        wifi_profile['ssid'] = profile_name
+        cmd_list_2 = "netsh wlan show profile".split() + [profile_name] + ["key=clear"]
+        password_re = re.search("Key Content            : (.*)\r", get_output(cmd_list_2))
+        if password_re == None:
+            wifi_profile["password"] = None
+        else:
+            wifi_profile["password"] = password_re[1] # [0] is the whole content
+    return wifi_profile
+
+all_wifi_profile_list = [get_full_wify_profile(profile_name) for profile_name in profile_names]
+
+with open('result.json', 'w') as fp:
+    json.dump(all_wifi_profile_list, fp)
+
