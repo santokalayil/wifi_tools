9ff2e25834025a10505634cdc016c6b6d3a6be1c
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-04-26 20:32:43 +0530

    committing files

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-from tools import get_wify_profile_passwords
-from tools import print_wifi_pass
+# from tools import get_wify_profile_passwords
+# from tools import print_wifi_pass
+
+from tools import get_all_devices_connected_to_network
